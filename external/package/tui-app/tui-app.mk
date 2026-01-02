ad18f6ab5b20821ecf0875b2e6ff3d456b64dad5
Feng Wang <wang6922@outlook.com>
2026-01-02 13:07:57 +0800

    update packages

--- external/package/tui-app/tui-app.mk
+++ external/package/tui-app/tui-app.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-TUI_APP_VERSION = 0.1.1
+TUI_APP_VERSION = 0.1.2
 TUI_APP_SITE = https://github.com/tiny-webui/webapp.git
 TUI_APP_SITE_METHOD = git
 TUI_APP_GIT_SUBMODULES = YES
